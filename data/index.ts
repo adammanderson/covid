55e9abaf730451b51437fd09645bce982f7ff7ea
Adam Anderson <adam.m.anderson@gmail.com>
2020-03-10 14:38:37 +0000

    Latest data 03/10, importAndParse function

--- data/index.ts
+++ data/index.ts
@@ -1,7 +1,13 @@
+const importAndParse = (file: string) => (
+  import(`${file}`)
+    .then((d) => JSON.parse(JSON.stringify(d)))
+);
+
 export default async () => ({
-  _20200305: await import('./20200305.json').then((d) => JSON.parse(JSON.stringify(d))),
-  _20200306: await import('./20200306.json').then((d) => JSON.parse(JSON.stringify(d))),
-  _20200307: await import('./20200307.json').then((d) => JSON.parse(JSON.stringify(d))),
-  _20200308: await import('./20200308.json').then((d) => JSON.parse(JSON.stringify(d))),
-  _20200309: await import('./20200309.json').then((d) => JSON.parse(JSON.stringify(d))),
+  _20200305: await importAndParse('./20200305.json'),
+  _20200306: await importAndParse('./20200306.json'),
+  _20200307: await importAndParse('./20200307.json'),
+  _20200308: await importAndParse('./20200308.json'),
+  _20200309: await importAndParse('./20200309.json'),
+  _20200310: await importAndParse('./20200310.json'),
 });
