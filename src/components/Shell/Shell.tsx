30e8600d4c6bf9d84367fcb5f33bb50e284fb3cb
Adam Anderson <adam.m.anderson@gmail.com>
2020-03-13 01:24:55 +0000

    Add twitter:og data

--- src/components/Shell/Shell.tsx
+++ src/components/Shell/Shell.tsx
@@ -12,6 +12,11 @@ const Shell: React.FC = ({
     <Head>
       <title>COVID-19 in the UK: Live data map</title>
       <meta name="description" content="The latest data map tracking COVID-19 in the UK." />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="og:url" content="https://covid19.tallo.io/" />
+      <meta property="og:title" content="COVID-19 UK Data Map" />
+      <meta property="og:description" content="Mapping coronavius (COVID-19) data across the UK." />
+      <meta property="og:image" content="https://covid19.tallo.io/covid.png" />
       <script async src="https://www.googletagmanager.com/gtag/js?id=UA-160202859-1" />
       <script
         dangerouslySetInnerHTML={{
