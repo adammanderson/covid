6c08d1e904e3e2fcd48a0a26ea27a56b94b7051c
Adam Anderson <adam.m.anderson@gmail.com>
2020-03-13 20:48:46 +0000

    Add 20200313 dataset

--- scraper/index.ts
+++ scraper/index.ts
@@ -1,12 +1,24 @@
-const SOURCE_TYPES = {
+const DATA_TYPES = {
   JSON: 'json',
   TABLE: 'table',
 };
 
-const EnglandAuthoritiesSource = {
-  type: SOURCE_TYPES.JSON,
-  endpoint: 'https://services1.arcgis.com/0IrmI40n5ZYxTUrV/arcgis/rest/services/CountyUAs_cases/FeatureServer/0/query?f=json&where=TotalCases%20%3C%3E%200&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=TotalCases%20desc&outSR=102100&resultOffset=0&resultRecordCount=1000&cacheHint=true',
-};
+const SOURCES = [
+  {
+    dataType: DATA_TYPES.JSON,
+    country: 'England',
+    locality: 'regions',
+    endpoint: 'https://services1.arcgis.com/0IrmI40n5ZYxTUrV/arcgis/rest/services/NHSR_Cases/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=TotalCases%20desc&outSR=102100&resultOffset=0&resultRecordCount=25&cacheHint=true',
+    parser: 'features[*].attributes.{label: NHSRNm, confirmed: TotalCases}',
+  },
+  {
+    dataType: DATA_TYPES.JSON,
+    country: 'England',
+    locality: 'authorities',
+    endpoint: 'https://services1.arcgis.com/0IrmI40n5ZYxTUrV/arcgis/rest/services/CountyUAs_cases/FeatureServer/0/query?f=json&where=TotalCases%20%3C%3E%200&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=TotalCases%20desc&outSR=102100&resultOffset=0&resultRecordCount=1000&cacheHint=true',
+    parser: 'features[*].attributes.{label: GSS_NM, confirmed: TotalCases}',
+  },
+];
 
 const EnglandRegionsSource = {};
 const ScotlandAuthoritiesSource = {};
